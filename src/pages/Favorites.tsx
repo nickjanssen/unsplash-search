831259e82fa83e23c03904928edbaf0cbd4ba524
Nick Janssen <info@nickjanssen.com>
2020-08-26 21:57:32 +0200

    feat(Favorites): list actual favorites

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { ImageResult } from "../components/Image";
+import Image, { ImageResult } from "../components/Image";
 import { AppState } from "../App";
 
 export interface FavoriteList {
@@ -17,13 +17,23 @@ interface Props {
 function Favorites(props: Props) {
   return (
     <div className="container mx-auto max-w-screen-lg">
-      <div className="m-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        {props.appState.lists.length > 0
-          ? props.appState.lists.map((list) => {
-              return <div>{list.title}</div>;
-            })
-          : <div>You haven't created any lists yet.</div>}
-      </div>
+      {props.appState.lists.length > 0 ? (
+        props.appState.lists.map((list) => {
+          return (
+            <div key={list.title}>
+              <div className="text-xl font-bold">{list.title}</div>
+              <div className="">{list.description}</div>
+              <div className="m-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                {list.images.map((image) => {
+                  return <Image key={image.id} image={image} {...props} />;
+                })}
+              </div>
+            </div>
+          );
+        })
+      ) : (
+        <div>You haven't created any lists yet.</div>
+      )}
     </div>
   );
 }
