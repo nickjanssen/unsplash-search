455fc1e6e36970059d712154f6f23f686e3833d8
Nick Janssen <info@nickjanssen.com>
2020-08-27 07:50:44 +0200

    feat(Favorites): allow editing of title and description

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -15,14 +15,40 @@ interface Props {
 }
 
 function Favorites(props: Props) {
+  const { dispatch } = props;
   return (
     <div className="container mx-auto max-w-screen-lg mt-5 p-5">
       {props.appState.lists.length > 0 ? (
         props.appState.lists.map((list) => {
           return (
             <div key={list.title}>
-              <div className="text-xl font-bold">{list.title}</div>
-              <div>{list.description}</div>
+              <div
+                className="text-xl font-bold"
+                contentEditable
+                suppressContentEditableWarning={true}
+                onBlur={(e) => {
+                  dispatch({
+                    type: "change-list-title",
+                    listTitle: list.title,
+                    newListTitle: e.target.innerText,
+                  });
+                }}
+              >
+                {list.title}
+              </div>
+              <div
+                contentEditable
+                suppressContentEditableWarning={true}
+                onBlur={(e) => {
+                  dispatch({
+                    type: "change-list-description",
+                    listTitle: list.title,
+                    newListDescription: e.target.innerText,
+                  });
+                }}
+              >
+                {list.description}
+              </div>
               <div className="mx-5 mt-5 mb-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
                 {list.images.map((image) => {
                   return <Image key={image.id} image={image} {...props} />;
